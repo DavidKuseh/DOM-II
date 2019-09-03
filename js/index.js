592ba00a7178fe6bb5170daa50adcf2dc7b60618
David Kuseh <davegru203@gmail.com>
2019-09-03 12:55:27 +0000

    added keydown event

--- js/index.js
+++ js/index.js
@@ -6,7 +6,17 @@ const navItem = document.querySelectorAll('.nav-link')
 navItem.forEach(item => item.addEventListener('mouseover', e => e.target.style.color = 'green')   
 );
 
+//keydown
 
+const logoHeading = document.querySelector('.logo-heading');
+
+window.addEventListener('keydown', keypressed, false);
+
+function keypressed(key) {
+    if(key.keyCode == "65"){
+        alert("a has been pressed");
+    }
+}
 
 
     
