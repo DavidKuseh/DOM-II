59754fdb14f03883a409348b4eb2648fba82d7c9
David Kuseh <davegru203@gmail.com>
2019-09-03 12:15:51 +0000

    created mouseover event

--- js/index.js
+++ js/index.js
@@ -1,1 +1,13 @@
 // Your code goes here
+
+//Mouseover
+const navItem = document.querySelectorAll('.nav-link')
+
+navItem.forEach(item => item.addEventListener('mouseover', e => e.target.style.color = 'green')   
+);
+
+
+
+
+    
+    
